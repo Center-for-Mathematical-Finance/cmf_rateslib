6f5b33dcb0bf343d561517fc23e3989b4b4adcc5
gurskyzhora <71128884+gurskyzhora@users.noreply.github.com>
2021-12-09 15:57:57 +0300

    Add files via upload

--- cmf_rateslib/rates/mean_reverting_pca.py
+++ cmf_rateslib/rates/mean_reverting_pca.py
@@ -2,19 +2,17 @@
 from ..rates.base_model import BaseRatesModel
 from ..curves.zero_curve import ZeroCurve
 import numpy as np
-from sympy.solvers import solve
-from sympy import Symbol
-import random
 
 
-class SimplePCAModel(BaseRatesModel):
+class MeanRevertingPCAModel(BaseRatesModel):
 
-    def __init__(self, maturities, maturity_loadings, factor_vols):
+    def __init__(self, maturities, maturity_loadings, factor_vols, theta):
         super().__init__()
 
         maturities = np.array(maturities)
         maturity_loadings = np.array(maturity_loadings)
         factor_vols = np.array(factor_vols)
+        theta = np.array(theta)
 
         if maturities.shape[0] != maturity_loadings.shape[0]:
             raise ValueError("Shapes of maturities and maturity_loadings don't match")
@@ -26,43 +24,89 @@ class SimplePCAModel(BaseRatesModel):
         self.params['loadings'] = maturity_loadings
         self.params['num_factors'] = maturity_loadings.shape[1]
         self.params['factor_vols'] = factor_vols
+        self.params['theta'] = theta
 
     def evolve_zero_curve(self, start_curve: ZeroCurve, num_periods: int, dt: float):
 
         U = self.params['loadings']
         num_factors = self.params['num_factors']
         sigma = self.params['factor_vols']
+        theta = self.params['theta']
+
 
         maturities = self.params['maturities']
 
         dW = np.random.randn(num_periods, num_factors) * np.sqrt(dt)
+        dX = []
 
-        # increments of PCA factors
-        dX = sigma * dW 
+        starting_rates = start_curve.zero_rate(maturities)
+
+        X = starting_rates[0]
+
+        for d in dW:
+            dX.append((np.random.randn(num_factors) * theta - X) * dt + sigma * d)
+            X += dX[-1]
+
+        dX = np.array(dX)
 
         # increments of zero rates
         dZ = U.dot(dX.T)
 
-        starting_rates = start_curve.zero_rate(maturities)
-
         all_rates = np.cumsum(np.concatenate([[starting_rates], dZ.T]), axis = 0)[1:, :]
-
 
         return [ZeroCurve(maturities, rates) for rates in all_rates]
 
-    def create_new(self, num: int, dt: float):
+
+    def create_new(self, num_periods: int, dt: float):
 
         U = self.params['loadings']
         num_factors = self.params['num_factors']
         sigma = self.params['factor_vols']
+        theta = self.params['theta']
+
+
         maturities = self.params['maturities']
 
+        dW = np.random.randn(num_periods, num_factors) * np.sqrt(dt)
+        dX = []
 
-        dW = np.random.randn(num, num_factors) * np.sqrt(dt)
-        dX = sigma * dW
+        X = 0
+
+        for d in dW:
+            dX.append((np.random.randn(num_factors) * theta - X) * dt + sigma * d)
+            X += dX[-1]
+
+        dX = np.array(dX)
+
+        # increments of zero rates
         dZ = U.dot(dX.T)
 
         return [ZeroCurve(maturities, rates) for rates in dZ.T]
+
+    def create_new_1(self, dt: float):
+        sigma = self.params['factor_vols']
+        theta = self.params['theta']
+        maturities = self.params['maturities']
+        alpha = 4
+        curves = []
+        for m in maturities:
+            for t in theta:
+                for s in sigma:
+                    u = np.random.randn(m * 253)
+                    dW = u * np.sqrt(dt)
+                    r_0 = t
+                    r_i = r_0
+                    r_t_mc = np.zeros(m * 253)
+                    r_t_mc[0] = r_0
+                    for i in range(1, m * 253):
+                        r_i += alpha * (t - r_i) * dt + s * dW[i]
+                        r_t_mc[i] = r_i
+                    curves.append(r_t_mc)
+
+        return curves
+
+
+
 
 
     def fit(self, curves):
@@ -78,31 +122,6 @@ class SimplePCAModel(BaseRatesModel):
         Cov = np.cov(curves_rates)
         eigvalues, eigvectors = np.linalg.eig(Cov)
         return eigvalues, eigvectors, U
-
-    #  пытался сам написать скореллированные кривые. Не спрослось( (не работает)
-    '''def gen_cor_curve(self, dt: float, cor: float):
-
-        vol = self.params['factor_vols']
-        maturities = self.params['maturities']
-
-        curv1 = []
-        cor_curves = []
-        for m in maturities:
-            for v in vol:
-                curv1.append(np.random.randn(m * 253) * np.sqrt(dt) * v)
-                curv_to_cor = np.random.randn(m * 253) * np.sqrt(dt) * v
-                x = Symbol('x')
-                tmp_curve = np.random.randn(m * 253) * np.sqrt(dt) * x
-                tmp = []
-                for i in range(m * 253):
-                    tmp.append((curv_to_cor[i] - curv_to_cor.mean()) * (tmp_curve[i] - tmp_curve.mean()))
-                tmp = np.array(tmp)
-                sought_std = solve(tmp.mean() / (v * x) - cor, x)
-                cor_curves.append(np.random.randn(m * 253) * np.sqrt(dt))
-
-        return sought_std, curv1, cor_curves'''
-
-
 
 
 
